bb5cb01b52a03e50b686720f28f29f6d1a8eb1e7
Ali Shahsamand <104649673+AliAzger@users.noreply.github.com>
2025-10-21 22:57:47 -0400

    Change database connection settings
    Updated database connection parameters for test environment.

--- database.php
+++ database.php
@@ -1,12 +1,21 @@
 <?php
+
+$servername = "172.25.15.188";
+$username =  "testUser";
+$password = "Jhmwty9810!";
+$dbname = "testdb";
+
+/*
 $servername = "172.25.199.66";
 $username =  "testUser";
 $password = "12345";
 $dbname = "testdb";
 
+ */
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error){
         die("connection failed: " . $conn->connect_error);
 }
 ?>
+
