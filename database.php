984ee3b42d362060a40b626f1ee518dde94382f0
Ali Shahsamand <104649673+AliAzger@users.noreply.github.com>
2025-10-29 20:25:54 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,11 @@
 <?php
+/*
+$servername = "172.25.15.188";
+$username =  "testUser";
+$password = "Jhmwty9810!";
+$dbname = "testdb";
+ 
+ */
 $servername = "172.25.199.66";
 $username =  "testUser";
 $password = "12345";
@@ -7,6 +14,6 @@ $dbname = "testdb";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error){
-        die("connection failed: " . $conn->connect_error);
+        die("connection failed:".$conn->connect_error);
 }
 ?>
