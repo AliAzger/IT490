2cc18aba581f4787862a4fae4b12268f7fb998ea
Ali Shahsamand <104649673+AliAzger@users.noreply.github.com>
2025-12-03 13:02:58 -0500

    md5 hash for registering
    Hash the password using md5 before processing.

--- register.php
+++ register.php
@@ -15,7 +15,7 @@ $queueName  = 'testQueue';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $password = md5($_POST['password'] ?? '');
 
     if (empty($username) || empty($password)) {
         echo "Username and password are required.";
