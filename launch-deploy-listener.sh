bc304330180bf7e5f4ad34028c4f721e02e84597
Dylan <dylankiddo10@gmail.com>
2025-12-08 14:07:09 -0500

    only run service after mysql and tailscale are up n running

--- launch-deploy-listener.sh
+++ launch-deploy-listener.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+
+echo "launch script run";
 
 cd /home/dylan/dev/IT490;
 source ./.venv/bin/activate;
+echo "python venv activated";
+
 cd deploy-system;
-python listener.py
+python listener.py;
